1a23b6246c69c83e1271c4035ac52d2a4194b7ac
Bruno.Castro <bruno.castro@mag-it.com.br>
2020-05-11 23:32:47 -0300

    adicionando snake no centro da tela

--- main.js
+++ main.js
@@ -1,3 +1,78 @@
 import * as THREE from './three.module.js';
 
-console.log('olá');
+main();
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+    const camera = createCamera(renderer);
+
+    const scene = new THREE.Scene();
+
+    scene.add(createLight());
+
+    scene.add(createSnake());
+
+    function render(time) {
+        time *= 0.001;  // convert time to seconds
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+        
+        renderer.render(scene, camera);
+        
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+}
+
+function createCamera(renderer){
+
+    const fov = 75;
+    const aspect = renderer.domElement.clientWidth / renderer.domElement.clientHeight;
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 5;
+
+    return camera;
+}
+
+function createLight(){
+
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+
+    return light;
+}
+
+function createSnake(){
+    
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    const material = new THREE.MeshPhongMaterial();
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.x = 0;
+
+    return cube;
+}
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
